d0ee26b0d08c28eec3a7c65ac8cb23682e168626
nekushi_cororo <airu219@gmail.com>
2019-03-09 16:05:21 +0900

    [update]商品一覧のデザイン修正
    テーブル、Input、ボタンなどの修正

--- view/cart.php
+++ view/cart.php
@@ -11,11 +11,13 @@
     <!-- JS -->
 
     <!-- CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/maechabin/bootstrap-material-button-color/master/dist/cb-materialbtn.0.5.5.min.css">
+
     <style>
     table { table-layout: fixed; }
     table th, table td { overflow: hidden; }
     </style>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">    <link rel="shortcut icon" href="">
 </head>
 <body>
     <!-- main -->
@@ -24,11 +26,12 @@
             <div class="jumbotron mt-4">
                 <h2 class="disply-4">商品一覧</h2>
                 <hr clas="my-4">
-                <!-- <div class="row my-5">
+                <div class="row">
                     <div class="col">
-                        <h3><a class="btn btn-primary btn-lg w-100" href="#" role="button">QRコード読み込み</a></h3>
+                        <p class="text-info">金額の入力間違いないようにご注意ください。</p>
                     </div>
                 </div>
+                <!-- 
                 <div class="row">
                     <div class="col">
                         <h3><a class="btn btn-primary btn-lg" href="./index.php" role="button">TOP</a></h3>
@@ -46,11 +49,11 @@
                     <table class="table">
                         <thead>
                             <tr>
-                                <th>#</th>
+                                <th style="width: 40px" scope="col">#</th>
                                 <th scope="col">商品名</th>
-                                <th>価格</th>
-                                <th>個数</th>
-                                <th>小計</th>
+                                <th style="width: 80px"scope="col">価格</th>
+                                <th style="width: 10%" scope="col">個数</th>
+                                <th style="width: 35%"scope="col">小計</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -73,30 +76,37 @@
                           <?php 
                             endif;
                             endforeach; ?>
-                            <tr class="table-info">
-                                <td colspan="3"></td>
-                                <td>税額</td>
+                            <tr >
+                                <td colspan="2"></td>
+                                <td colspan="2" class="table-info">税額</td>
                                 <td>￥<?php echo intval($sum*0.08); ?></td>
                             </tr>
                             <tr >
-                                <td colspan="3"></td>
-                                <td class="table-info">合計金額</td>
+                                <td colspan="2"></td>
+                                <td colspan="2" class="table-info">合計金額</td>
                                 <td>￥<?php echo intval($sum*1.08); ?></td>
                             </tr>
                             <tr>
-                                <td colspan="3"></td>
-                                <td class="table-info">お預かり金額</td>
-                                <td>
-                                <div class="input-group mb-3">
-                                    
+                                <td colspan="2"></td>
+                                <td colspan="2" class="table-info">お預かり金額</td>
+                                <td rowspan="">
                                     <form method="post" action="?page=scart">
-                                        <div class="input-group-prepend">
-                                            <span style="" class="input-group-text" id="basic-addon1">￥</span>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span style="" class="input-group-text" id="basic-addon1">￥</span>
+                                            </div>
+                                            <input type="number" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1" name="Liq">
                                         </div>
-                                      <input type="number" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1" name="Liq">
-                                      <input type="submit" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1" name="sub" value="清算">
+                                </td>
+                            </tr>
+                            <tr >
+                                <td colspan="2"></td>
+                                <td colspan="2"></td>
+                                <td>
+                                        <div class="form-group">
+                                            <input type="submit" class="form-control btn btn-cyan500_rsd" placeholder="" aria-label="" aria-describedby="basic-addon1" name="sub" value="清算">
+                                        </div>
                                     </form>
-                                </div>
                                 </td>
                             </tr>
                         </tbody>
